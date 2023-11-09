8851e178c433db2d95f3d24e5940465d0209dd33
Joey Groves <link_groves@yahoo.co.uk>
2023-11-09 18:36:00 +0000

    added useMediaQuery hook

--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -1,3 +1,21 @@
 // determines if you match a 'breakpoint' of a media screensize
 
 import { useState, useEffect } from "react";
+
+const useMediaQuery = query => {
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+        const media = window.matchMedia(query);
+        if (media.matches !== matches) {
+            setMatches(media.matches);
+        }
+        const listener = () => setMatches(media.matches);
+        window.addEventListener("resize", listener);
+        return () => window.removeEventListener("resize", listener);
+    }, [matches, query]);
+
+    return matches;
+}
+
+export default useMediaQuery;
