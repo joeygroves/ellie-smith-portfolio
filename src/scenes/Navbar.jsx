e31bca2376267031449291355fa55709e00b359a
Joey Groves <link_groves@yahoo.co.uk>
2023-11-10 18:41:54 +0000

    added navbar background

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -19,9 +19,10 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-green";
 
     return (
-        <nav className={`z-40 w-full fixed top-0 py-6`}>
+        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
                 <h4 className="font-montserrat text-3xl font-black">Ellie Smith</h4>
                 {/* DESKTOP NAV*/}
@@ -29,6 +30,11 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
                     <div className="flex justify-between gap-16 font-montserrat text-sm font-semibold">
                         <Link
                             page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="About"
                             selectedPage={selectedPage}
                             setSelectedPage={setSelectedPage}
                         />
