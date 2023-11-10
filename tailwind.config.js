dcd4d0356763839d47b053bb6f36b6630d3077f3
Joey Groves <link_groves@yahoo.co.uk>
2023-11-10 00:01:09 +0000

    configured tailwindcss theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js, jsx}"],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#F2EFF2",
+        lavender: "#AE9FD0",
+        green: "#519E8A",
+        yellow: "#FFCA3A",
+        black: "#14080E"
+      },
+      backgroundImage: (theme) => ({
+        purple: "#A875BA"
+      }),
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        abel: ["Abel", "sans-serif"]
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    }
   },
   plugins: [],
 }
