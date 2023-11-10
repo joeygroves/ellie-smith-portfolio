7af337798b2d8bacc85c137d51b0cb3c7f641b56
Joey Groves <link_groves@yahoo.co.uk>
2023-11-10 20:34:22 +0000

    added navbar background when not top of page

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -19,9 +19,10 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-green";
 
     return (
-        <nav className={`z-40 w-full fixed top-0 py-6`}>
+        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
                 <h4 className="font-montserrat text-3xl font-black">Ellie Smith</h4>
                 {/* DESKTOP NAV*/}
