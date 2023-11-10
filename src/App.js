7c82c3449b25aa677ebc533ff2c9e484cd6281dc
Joey Groves <link_groves@yahoo.co.uk>
2023-11-10 18:29:02 +0000

    fixed tailwind config

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home');
-  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const [selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) setIsTopOfPage(true);
-      if (window.scrollY !== 0) setIsTopOfPage(false);
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  })
 
   return (
     <div className="app bg-purple">
       <Navbar 
-        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -2,15 +2,40 @@ import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+    const lowerCasePage = page.toLowerCase();
+    return (
+      <AnchorLink
+        className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+                  hover:text-yellow transition duration-500`}
+        href={`#${lowerCasePage}`}
+        onClick={() => setSelectedPage(lowerCasePage)}
+      >
+        {page}
+      </AnchorLink>
+    );
+  };
+
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     const [isMenuToggled, setIsMenuToggled] = useState(false);
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-    const navbarBackground = isTopOfPage ? "" : "bg-green";
 
     return (
-        <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+        <nav className={`z-40 w-full fixed top-0 py-6`}>
             <div className="flex items-center justify-between mx-auto w-5/6">
                 <h4 className="font-montserrat text-3xl font-black">Ellie Smith</h4>
+                {/* DESKTOP NAV*/}
+                {isAboveSmallScreens ? (
+                    <div className="flex justify-between gap-16 font-montserrat text-sm font-semibold">
+                        <Link
+                            page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                    </div>
+                ) : (<div></div>
+                )}
+
             </div>
         </nav>
     );
