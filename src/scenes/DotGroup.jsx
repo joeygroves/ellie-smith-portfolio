3ea3ae00385f23bd822379accf11a4a4706bb219
Joey Groves <link_groves@yahoo.co.uk>
2023-11-10 20:47:38 +0000

    created dot button for each page

--- src/scenes/DotGroup.jsx
+++ src/scenes/DotGroup.jsx
@@ -1,7 +1,48 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const DotGroup = ({ selectedPage, setSelectedPage }) => {
+    const selectedStyles = `relative bg-yellow before:absolute before:w-6 before:h-6 
+    before:rounded-full before:border-2 before:border-yellow before:left-[-50%]
+    before:top-[-50%]`;
 
+    return (
+        <div className="flex flex-col gap-6 fixed top-[60%] right-7">
+            <AnchorLink
+                className={`${selectedPage === "home" ? selectedStyles : "bg-white"}
+                   w-3 h-3 rounded-full`}
+                href="#home"
+                onClick={() => setSelectedPage("home")}
+            />
+
+            <AnchorLink
+                className={`${selectedPage === "about" ? selectedStyles : "bg-white"}
+                    w-3 h-3 rounded-full`}
+                href="#about"
+                onClick={() => setSelectedPage("about")}
+            />
+
+            <AnchorLink
+                className={`${selectedPage === "affiliates" ? selectedStyles : "bg-white"}
+                    w-3 h-3 rounded-full`}
+                href="#affiliates"
+                onClick={() => setSelectedPage("affiliates")}
+            />
+
+            <AnchorLink
+                className={`${selectedPage === "testimonials" ? selectedStyles : "bg-white"}
+                    w-3 h-3 rounded-full`}
+                href="#testimonials"
+                onClick={() => setSelectedPage("testimonials")}
+            />
+
+            <AnchorLink
+                className={`${selectedPage === "contact" ? selectedStyles : "bg-white"}
+                    w-3 h-3 rounded-full`}
+                href="#contact"
+                onClick={() => setSelectedPage("contact")}
+            />
+        </div>
+    )
 }
 
 export default DotGroup;
