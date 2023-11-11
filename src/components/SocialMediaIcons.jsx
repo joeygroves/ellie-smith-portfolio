43e6675c57970431677195f3c980b40836073f70
Joey Groves <link_groves@yahoo.co.uk>
2023-11-11 17:37:32 +0000

    added icons and temp links for youtube and instagram

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,6 +1,26 @@
 
 const SocialMediaIcons = () => {
-    
-}
+    return (
+        <div className="flex justify-center md:justify-start my-10 gap-7">
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.youtube.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="youtube-link" src="../assets/youtube.svg" />
+            </a>
+
+            <a
+                className="hover:opacity-50 transition duration-500"
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img alt="instagram-link" src="../assets/instagram.svg" />
+            </a>
+        </div>
+    );
+};
 
 export default SocialMediaIcons;
