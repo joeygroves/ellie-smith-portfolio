56269e2aba4c66a089f5eabe1a2ed395a638d90b
Joey Groves <link_groves@yahoo.co.uk>
2023-11-11 17:20:59 +0000

    started work on social media icon component

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -1,3 +1,4 @@
+import SocialMediaIcons from "../components/SocialMediaIcons";
 import useMediaQuery from '../hooks/useMediaQuery';
 import { motion } from 'framer-motion';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
@@ -7,6 +8,11 @@ const Landing = ({setSelectedPage}) => {
 
     return (
         <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
+            {/* Embeded YouTube Video Section */}
+            <div>
+
+            </div>
+
             {/* Main Section */}
             <div className="z-30 basis-2/5 mt-12 md:mt-32">
                 {/* Headings */}
@@ -42,27 +48,30 @@ const Landing = ({setSelectedPage}) => {
                         visible: { opacity: 1, x: 0 },
                     }}
                 >
-                <AnchorLink
-                    className="bg-green text-deep-blue rounded-sm py-3 px-7 font-abel font-semibold
-                    hover:bg-blue hover:text-black transition duration-500 text-lg"
-                    onClick={() => setSelectedPage("contact")}
-                    href="#contact"
-                >
-                    Contact Me
-                </AnchorLink>
+                    <AnchorLink
+                        className="bg-green text-deep-blue rounded-sm py-3 px-7 font-abel font-semibold
+                        hover:bg-blue hover:text-black transition duration-500 text-lg"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        Contact Me
+                    </AnchorLink>
 
-                <AnchorLink
-                    className="rounded-r-sm bg-yellow py-0.5 pr-0.5 pl-0.5"
-                    onClick={() => setSelectedPage("contact")}
-                    href="#contact"
-                >
-                    <div className="bg-purple text-white hover:text-black transition duration-500 w-full h-full flex items-center justify-center px-10 font-abel text-lg">
-                    Let's talk.
-                    </div>
-                </AnchorLink>
+                    <AnchorLink
+                        className="rounded-r-sm bg-yellow py-0.5 pr-0.5 pl-0.5"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        <div className="bg-purple text-white hover:text-black transition duration-500 w-full h-full flex items-center justify-center px-10 font-abel text-lg">
+                        Let's talk.
+                        </div>
+                    </AnchorLink>
                 </motion.div>
 
                 {/* Social Media Icons */}
+                <motion.div>
+                    <SocialMediaIcons />
+                </motion.div>
             </div>
         </section>
     )
