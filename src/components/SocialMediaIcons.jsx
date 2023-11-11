f453865b5a8f5f6db029762b06bd1bf881ca17a9
Joey Groves <link_groves@yahoo.co.uk>
2023-11-11 20:14:51 +0000

    updated temp links to actual social media links

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -5,7 +5,7 @@ const SocialMediaIcons = () => {
         <div className="flex justify-center md:justify-start my-10 gap-7">
             <a
                 className="hover:opacity-50 transition duration-500"
-                href="https://www.youtube.com"
+                href="https://www.youtube.com/@elliedee5221"
                 target="_blank"
                 rel="noreferrer"
             >
@@ -14,7 +14,7 @@ const SocialMediaIcons = () => {
 
             <a
                 className="hover:opacity-50 transition duration-500"
-                href="https://www.instagram.com"
+                href="https://www.instagram.com/ellie.deeee"
                 target="_blank"
                 rel="noreferrer"
             >
@@ -23,7 +23,7 @@ const SocialMediaIcons = () => {
 
             <a
                 className="hover:opacity-50 transition duration-500"
-                href="https://www.x.com"
+                href="https://www.x.com/elliedeeee"
                 target="_blank"
                 rel="noreferrer"
             >
@@ -32,7 +32,7 @@ const SocialMediaIcons = () => {
 
             <a
                 className="hover:opacity-50 transition duration-500"
-                href="https://www.tiktok.com"
+                href="https://www.tiktok.com/@elliedeeee"
                 target="_blank"
                 rel="noreferrer"
             >
