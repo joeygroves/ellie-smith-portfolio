7c4c5c1881513321af060b826a9695137302fedc
Joey Groves <link_groves@yahoo.co.uk>
2023-11-15 13:42:49 +0000

    added heading

--- src/scenes/About.jsx
+++ src/scenes/About.jsx
@@ -1,6 +1,27 @@
+import LineGradient from '../components/LineGradient';
+import useMediaQuery from '../hooks/useMediaQuery';
+import { motion } from 'framer-motion';
 
 const About = () => {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1024px)");
 
-}
+    return (
+        <section
+            id="about"
+            className="pt-10 pb-24"
+        >
+            {/* Header and Image Section */}
+            <div className="md:flex md:justify-between md:gap-16 mt-32">
+                <motion.div>
+                    <p className="font-montserrat font-black text-4xl mb-5">
+                        <span className="text-yellow">About</span> Ellie
+                    </p>
+                    <LineGradient width="w-1/2" />
+                </motion.div>
+            </div>
+
+        </section>
+    );
+};
 
 export default About;
