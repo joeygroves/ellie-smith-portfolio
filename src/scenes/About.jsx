905c07defbb39f25d7e4a8fa453e07b12730f990
Joey Groves <link_groves@yahoo.co.uk>
2023-11-15 23:14:43 +0000

    added animations for about bio block text

--- src/scenes/About.jsx
+++ src/scenes/About.jsx
@@ -27,21 +27,32 @@ const About = () => {
                         <span className="text-yellow">About</span> Ellie
                     </p>
                     <LineGradient width="w-1/2" />
-                    
-                    <div className="mt-10 mb-7">
-                        Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
-                        fames odio in at.<br />
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 },
+                            }}
+                    >
+                        <div className="mt-10 mb-7">
+                            Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
+                            fames odio in at.<br />
 
-                        <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                        tempor incididunt ut labore et dolore magna aliqua. Blandit aliquam etiam 
-                        erat velit. Sagittis nisl rhoncus mattis rhoncus urna. 
-                        Condimentum lacinia quis vel eros donec ac odio. <br />
-                        
-                        <br />Egestas maecenas pharetra 
-                        convallis posuere morbi leo urna molestie at. Fames ac turpis egestas sed 
-                        tempus urna et pharetra pharetra. 
-                    </div>
+                            <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                            tempor incididunt ut labore et dolore magna aliqua. Blandit aliquam etiam 
+                            erat velit. Sagittis nisl rhoncus mattis rhoncus urna. 
+                            Condimentum lacinia quis vel eros donec ac odio. <br />
+                                
+                            <br />Egestas maecenas pharetra 
+                            convallis posuere morbi leo urna molestie at. Fames ac turpis egestas sed 
+                            tempus urna et pharetra pharetra. 
+                        </div>
+                    </motion.div>
                 </motion.div>
+                    
 
                 <div className="mt-16 md:mt-0">
                     {isAboveMediumScreens ? (
