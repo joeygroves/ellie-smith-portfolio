8fd04da7955e67a43f5243cce30ed026e02a04de
Joey Groves <link_groves@yahoo.co.uk>
2023-11-15 13:43:28 +0000

    added framer animation to heading

--- src/scenes/About.jsx
+++ src/scenes/About.jsx
@@ -12,7 +12,17 @@ const About = () => {
         >
             {/* Header and Image Section */}
             <div className="md:flex md:justify-between md:gap-16 mt-32">
-                <motion.div>
+                <motion.div
+                    className="md:w-1/3"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 },
+                }}
+                >
                     <p className="font-montserrat font-black text-4xl mb-5">
                         <span className="text-yellow">About</span> Ellie
                     </p>
