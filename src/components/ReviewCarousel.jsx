b4ebdfce8d0fcf8beaf85cbe16782d8c513b752f
Joey Groves <link_groves@yahoo.co.uk>
2023-11-17 21:13:19 +0000

    added example testimonials data

--- src/components/ReviewCarousel.jsx
+++ src/components/ReviewCarousel.jsx
@@ -1,6 +1,28 @@
 
-const ReviewCarousel = () => {
+const testimonialsData = [
+    {
+      id: 1,
+      name: 'John Wills',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur non earum repudiandae ipsa reprehenderit quaerat, quo a cupiditate, voluptatibus voluptates, dolores nam accusantium error, modi iure itaque totam labore.'
+    },
+    {
+      id: 2,
+      name: 'Jasmine Perry',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, magni fugiat eveniet minus perspiciatis! Nostrum laborum maxime consequuntur repellat nam magni, quae incidunt distinctio enim itaque eligendi laboriosam, quod, ad!'
+    },
+    {
+      id: 3,
+      name: 'Rocky Johnson',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium facilis optio porro omnis alias eaque corporis error est ut, reprehenderit quae asperiores illum quo voluptates debitis non. Repellat fugit, asperiores?'
+    }
+]
 
-}
+const ReviewCarousel = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
 
 export default ReviewCarousel;
