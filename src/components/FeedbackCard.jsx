85dff401870b3e01d56373a79c489d169f4e9fd9
Joey Groves <link_groves@yahoo.co.uk>
2023-11-21 07:40:37 +0000

    finished creating feedback card component

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,6 +1,25 @@
 
-const FeedbackCard = () => {
+const FeedbackCard = ({ description, name, title }) => {
+    return (
+        <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] mx-auto my-0 feedback-card">
+            {/* Testimonial Description */}
+            <p className="font-montserrat font-semibold italic text-[18px] leading-[32.4px] text-white my-10">
+                “{description}”
+            </p>
 
-}
+            {/* Name and Title */}
+            <div className="flex flex-row">
+                <div className="flex flex-col ml-4">
+                    <h4 className="font-montserrat font-normal text-[20px] leading-[32px] text-white">
+                        {name}
+                    </h4>
+                    <p className="font-montserrat font-normal italic text-[13px] leading-[24px] text-white">
+                        {title}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default FeedbackCard;
