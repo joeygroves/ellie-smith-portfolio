16d435786bd83201c1d569c679a7ce5fda0998d8
Joey Groves <link_groves@yahoo.co.uk>
2023-11-21 08:55:57 +0000

    imported register and trigger validation from react-hook-form

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -3,7 +3,18 @@ import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
 
 const Contact = () => {
+    const {
+        register,
+        trigger,
+        formState: {errors}
+    } = useForm();
 
+    const onSubmit = async (e) => {
+        const isValid = await trigger();
+        if (!isValid) {
+            e.preventDefault();
+        }
+    }
 }
 
 export default Contact;
