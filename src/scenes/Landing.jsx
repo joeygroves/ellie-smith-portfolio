61f70a2ea4efb5280a1af6e6243b129592c0b72e
Joey Groves <link_groves@yahoo.co.uk>
2023-11-24 17:45:56 +0000

    fixed responsive width bugs

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Landing = ({setSelectedPage}) => {
-    const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1375px)");
 
     return (
         <section 
