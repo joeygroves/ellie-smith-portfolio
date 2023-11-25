e10586678c82bb1b2c3c518b11ecc4bcceab7e19
Joey Groves <link_groves@yahoo.co.uk>
2023-11-25 15:15:30 +0000

    edited link to images

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,44 +1,43 @@
 // social media icon component
 
 const SocialMediaIcons = () => {
-    const imageSize = `scale-50`
 
     return (
         <div className="flex justify-center md:justify-start my-10 gap-7">
             <a
-                className="hover:opacity-50 transition duration-500"
+                className="hover:opacity-50 transition duration-500 px-1"
                 href="https://www.youtube.com/@elliedee5221"
                 target="_blank"
                 rel="noreferrer"
             >
-                <img alt="youtube-link" src="../assets/youtube.svg" className={imageSize}/>
+                <img alt="youtube-link" src="../assets/youtube-web.svg" />
             </a>
 
             <a
-                className="hover:opacity-50 transition duration-500"
+                className="hover:opacity-50 transition duration-500 px-1"
                 href="https://www.instagram.com/ellie.deeee"
                 target="_blank"
                 rel="noreferrer"
             >
-                <img alt="instagram-link" src="../assets/instagram.svg" className={imageSize} />
+                <img alt="instagram-link" src="../assets/instagram-web.svg"  />
             </a>
 
             <a
-                className="hover:opacity-50 transition duration-500"
+                className="hover:opacity-50 transition duration-500 px-1"
                 href="https://www.x.com/elliedeeee"
                 target="_blank"
                 rel="noreferrer"
             >
-                <img alt="x-link" src="../assets/x.svg" className={imageSize} />
+                <img alt="x-link" src="../assets/x-web.svg"  />
             </a>
 
             <a
-                className="hover:opacity-50 transition duration-500"
+                className="hover:opacity-50 transition duration-500 px-1"
                 href="https://www.tiktok.com/@elliedeeee"
                 target="_blank"
                 rel="noreferrer"
             >
-                <img alt="tiktok-link" src="../assets/tiktok.svg" className={imageSize} />
+                <img alt="tiktok-link" src="../assets/tiktok-web.svg"  />
             </a>
         </div>
     );
